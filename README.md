da92de845718db3b051e72fc41ae8edf61c14601
Matthew Schneider <59853199+mpschneider2@users.noreply.github.com>
2024-12-03 17:51:27 -0800

    most features except saving and restoring old times

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SpeedClimbTimer
 
 How it should work: holding button 1 for 5 seconds sets the clock to 0, releasing the long hold (onebutton) after the 5 seconds starts the timer
-Hitting button 2 should pause the timer and it should blink slowly if not a high score, strobe if a high score
+Hitting button 2 should pause the timer and it should blink slowly if not a high score, strobe if a high score. Maybe add a buzzer
 
 it should continue blinking until button 1 is pressed/clicked, where it should then show the high score in solid light.
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,92 @@
 #include "SevSeg.h"
+#include "OneButton.h"
 SevSeg sevseg; //Initiate a seven segment controller object
 
+unsigned long timerStart = 0;
+int timer = 0;
+int highScore = 222;
+
+int Button2 = A0;
+
+int DisplayState = 0;
+//0 is highscore, 1 is 00:00, 2 is timer, 3 is blinking time at end, 4 is blinking time NEW HIGH SCORE
+
+OneButton Button1 = OneButton (
+    A1,
+    true,
+    true
+);
+
+unsigned long ButtonHitMillis = 0;
 
 void setup() {
     byte numDigits = 4;  
     byte digitPins[] = {2, 3, 4, 5};
     byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
     bool resistorsOnSegments = 0; 
+
+    pinMode(Button2, INPUT_PULLUP);
+
     // variable above indicates that 4 resistors were placed on the digit pins.
     // set variable to 1 if you want to use 8 resistors on the segment pins.
     sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins, resistorsOnSegments);
     sevseg.setBrightness(90);
+
+    Button1.attachPress([]() {
+        if (DisplayState != 2) DisplayState = 0;
+    });
+
+    Button1.attachLongPressStart([]() {
+        DisplayState = 1;
+    });
+
+    Button1.attachLongPressStop([]() {
+        DisplayState = 2;
+        timerStart = millis();
+    });
+
+    Button1.setPressMs(3000);
+}
+
+void runTimer() {
+    timer = (millis()-timerStart)/10;
 }
 
 void loop() {
-    sevseg.setNumber(millis()/10, 2);
+    if (DisplayState==2) {
+        runTimer();
+        sevseg.setNumber(timer, 2);
+    } else if (DisplayState==3) {
+        if (timer <= highScore) {
+            highScore = timer;
+
+            if ((millis()-ButtonHitMillis)/10%30 < 20) {
+                sevseg.setNumber(timer, 2);
+            } else {
+                sevseg.blank();
+            }
+
+        } else {
+            if ((millis()-ButtonHitMillis)/10%150 < 125) {
+                sevseg.setNumber(timer, 2);
+            } else {
+                sevseg.blank();
+            }
+        }
+        
+    } else if (DisplayState == 0) {
+        sevseg.setNumber(highScore, 2);
+    } else if (DisplayState == 1) {
+        timer = 0;
+        sevseg.setNumber(timer, 2);
+    }
+
     sevseg.refreshDisplay(); // Must run repeatedly
+
+    if (digitalRead(Button2)==LOW) {
+        DisplayState = 3;
+        ButtonHitMillis = millis();
+    }
+
+    Button1.tick();
 }
